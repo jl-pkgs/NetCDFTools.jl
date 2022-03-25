c159231258f10438af6d369cc0ce120e0299b922
Dongdong Kong <kongdd@users.noreply.github.com>
2022-03-25 22:49:53 +0800

    add parameter `type` to `nc_read`

--- src/nc_read.jl
+++ src/nc_read.jl
@@ -7,7 +7,7 @@
 - `raw`: boolean. It `true`, not replace na values.
 - `period`: `[year_start, year_end]`
 """
-function nc_read(file, band = nothing; period = nothing, raw = false)
+function nc_read(file, band = nothing; type = nothing, period = nothing, raw = false)
 
     ds = Dataset(path_mnt(file))
     if band === nothing
@@ -19,16 +19,23 @@ function nc_read(file, band = nothing; period = nothing, raw = false)
     @time if period === nothing
         data = data[:]
     else
+        if length(period) == 1; period = repeat([period], 2); end
+        # change NC into Raster
         dates = ds["time"]
         years = Dates.year.(dates)
         ind = (years .>= period[1] .&& years .<= period[2]) |> findall
         # `ind` is continuous, but reading speed is faster when converting to `unitRange`
         # https://alexander-barth.github.io/NCDatasets.jl/stable/performance/
         ind = ind[1]:ind[end]
+        # @show ind
         data = data[:, :, ind]
         # data = length(dates) != length(ind) ?  data[:, :, ind] : data
     end
     close(ds)
+
+    if type !== nothing && eltype(data) != type
+        data = @.(type(data))
+    end
     data
     # data = SharedArray(data)
 end
