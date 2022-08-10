cbedf9aae4c97f5a7bb35a79981232601658ea9d
Dongdong Kong <kongdd@users.noreply.github.com>
2022-08-11 01:45:41 +0800

    add param `ind` to nc_read

--- src/nc_read.jl
+++ src/nc_read.jl
@@ -1,41 +1,49 @@
-# - return: data with the dimension of `[lon, lat, ntime]`
+"""
+    $(TYPEDSIGNATURES)
+
+# Arguments
+
+- `band`  : string (variable name) or int (band id). If `int` provided, 
+            `bandName = nc_bands(file)[band]`
+
+- `type`  : returned data type, e.g. `Float32`
+
+- `period`: `[year_start, year_end]` or `year`, time should be in the 3rd dimension.
+
+- `ind`   : if `ind` provided, `period` will be ignored.
+
+- `raw`   : boolean. It `true`, not replace na values.
 
 """
-    nc_read(file; band = nothing, period = nothing, raw = false)
-
-# Parameters
-- `raw`: boolean. It `true`, not replace na values.
-- `period`: `[year_start, year_end]`
-"""
-function nc_read(file, band = nothing; type = nothing, period = nothing, raw = false)
+function nc_read(file, band = 1; type = nothing, period = nothing, 
+    ind = nothing, raw = false)
 
     ds = Dataset(path_mnt(file))
-    if band === nothing
-        band = nc_bands(file)[1]
-    end
-
-    # @time data = ds[band].var[:] # not replace na values at here
-    data = raw ? ds[band].var : ds[band]
-    @time if period === nothing
-        data = data[:]
-    else
+    bandName = get_bandName(file, band)
+    # @time data = ds[bandName].var[:] # not replace na values at here
+    data = raw ? ds[bandName].var : ds[bandName]
+    
+    if ind === nothing && period !== nothing
         if length(period) == 1; period = repeat([period], 2); end
         # change NC into Raster
         dates = ds["time"]
         years = Dates.year.(dates)
-        ind = (years .>= period[1] .&& years .<= period[2]) |> findall
+        itime = (years .>= period[1] .&& years .<= period[2]) |> findall
         # `ind` is continuous, but reading speed is faster when converting to `unitRange`
         # https://alexander-barth.github.io/NCDatasets.jl/stable/performance/
-        ind = ind[1]:ind[end]
-        # @show ind
-        data = data[:, :, ind]
+        ind = (:, :, itime[1]:itime[end])
         # data = length(dates) != length(ind) ?  data[:, :, ind] : data
     end
+    if ind === nothing; ind = (:, ); end
+    data = data[ind...]
     close(ds)
-
+    
     if type !== nothing && eltype(data) != type
         data = @.(type(data))
     end
     data
     # data = SharedArray(data)
 end
+
+get_bandName(file, band::Integer) = nc_bands(file)[band]
+get_bandName(file, band::AbstractString) = band
