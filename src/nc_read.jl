c6cb64f39841dc94a5852780ce116aa5d7d5984b
Dongdong Kong <kongdd@users.noreply.github.com>
2023-02-16 15:11:51 +0800

    add trycatch to `nc_read`

--- src/nc_read.jl
+++ src/nc_read.jl
@@ -22,7 +22,6 @@ function nc_read(file, band=1;
   ds = Dataset(path_mnt(file))
   bandName = get_bandName(file, band)
   # @time data = ds[bandName].var[:] # not replace na values at here
-  data = raw ? ds[bandName].var : ds[bandName]
 
   if ind === nothing && period !== nothing
     if length(period) == 1
@@ -40,14 +39,20 @@ function nc_read(file, band=1;
   if ind === nothing
     ind = (:,)
   end
-  verbose && @show(data, ind)
 
-  data = data[ind...]
-  close(ds)
+  try
+    data = raw ? ds[bandName].var : ds[bandName]
+    
+    verbose && @show(data, ind) # debug mode
+    data = data[ind...]
 
-  if type !== nothing && eltype(data) != type
-    data = @.(type(data))
+    if type !== nothing && eltype(data) != type
+      data = @.(type(data))
+    end
+    return data
+  catch
+    @warn "failed to read file: $file"
+  finally
+    close(ds)
   end
-  data
-  # data = SharedArray(data)
 end
