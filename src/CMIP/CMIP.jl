3f2baa4f3921c5c53399481811c11345992ab9e0
Dongdong Kong <kongdd@users.noreply.github.com>
2023-10-06 14:00:31 +0800

    improve `map_df`

--- src/CMIP/CMIP.jl
+++ src/CMIP/CMIP.jl
@@ -38,9 +38,9 @@ function cbind(x::AbstractDataFrame; kw...)
   # end
 end
 
-function map_df(fun::Function, lst::GroupedDataFrame{DataFrame})
+function map_df(fun::Function, lst::GroupedDataFrame{DataFrame}, args...; kw...)
   n = length(lst)
-  map(i -> fun(lst[i]), 1:n)
+  map(i -> fun(lst[i], args...; kw...), 1:n)
 end
 
 include("esgf.jl")
