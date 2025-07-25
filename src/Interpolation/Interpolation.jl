f15efd2c1136882bef5951a93da58d85ccf92bdd
Dongdong Kong <kongdd@users.noreply.github.com>
2025-07-25 15:49:27 +0800

    rm earth_dist

--- src/Interpolation/Interpolation.jl
+++ src/Interpolation/Interpolation.jl
@@ -4,6 +4,4 @@ include("utilize.jl")
 include("bilinear_helper.jl")
 include("bilinear.jl")
 
-export angle_azimuth, angle_azimuth_sphere
-export earth_dist
 export bilinear
--- src/Interpolation/utilize.jl
+++ src/Interpolation/utilize.jl
@@ -1,4 +1,4 @@
-export rm_empty, weighted_nanmean!, weighted_nanmean, earth_dist
+export rm_empty, weighted_nanmean!, weighted_nanmean
 import Ipaper: weighted_nanmean
 
 # byrow
@@ -36,32 +36,4 @@ end
 
 function rm_empty(list::Vector)
   filter(x -> !isempty(x), list)
-end
-
-
-"""
-    earth_dist(x1::Matrix, x2::Matrix; R=6378.388)
-    earth_dist(p1::Tuple{FT,FT}, x2::AbstractMatrix; R=6378.388) where {FT}
-
-```julia
-p1 = [110 30; 111 31]
-p2 = [113 32; 115 35]
-earth_dist(p1, p2)
-```
-"""
-function earth_dist(x1::Matrix, x2::AbstractMatrix; R=6378.388)
-  lon1 = deg2rad.(x1[:, 1])
-  lat1 = deg2rad.(x1[:, 2])
-  lon2 = deg2rad.(x2[:, 1])
-  lat2 = deg2rad.(x2[:, 2])
-
-  _x = @. [cos(lat1) * cos(lon1) cos(lat1) * sin(lon1) sin(lat1)]
-  _y = @. [cos(lat2) * cos(lon2) cos(lat2) * sin(lon2) sin(lat2)]
-  pp = _x * _y'
-  return R .* acos.(clamp.(pp, -1, 1))
-end
-
-function earth_dist(p1::Tuple{FT,FT}, x2::AbstractMatrix; R=6378.388) where {FT}
-  x1 = [p1[1] p1[2]]
-  earth_dist(x1, x2; R)[:]
 end
